3b59d26175acca3600720c53b476bc6fc6c1455c
Нурсултан Осмоналиев <nursultanosmonaliev@Mac.lan>
2026-04-30 02:04:05 +0600

    fix vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -11,7 +10,6 @@ export default defineConfig({
   },
   preview: {
     host: '0.0.0.0',
-    port: 8080,
-    strictPort: true
+    port: 8080
   }
 })
